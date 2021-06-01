ba91756dac44b6dc8bc6fe1645c02189cfc0b1d9
Emil Kantis <emil.kantis@protonmail.com>
2021-06-01 23:58:42 +0200

    More descriptive logging

--- src/test/kotlin/MyTest.kt
+++ src/test/kotlin/MyTest.kt
@@ -9,16 +9,24 @@ class MyTest : FunSpec(
 
             test("Things should not take too much time") {
                 val aliased = MyTypeAlias(5) { false }
+                var logEveryReptition = false
+                var time = 0L
 
                 for (i in 1..1_000_000) {
-                    if (i % 10_000 == 0) println(i)
+                    if (i % 10_000 == 0 || logEveryReptition) println("Invocation ${i - 1} finished in ${time}ms")
 
-                    val time = measureTimeMillis {
+                    time = measureTimeMillis {
                         aliased.doFunThings()
+                    }
+
+                    if (i > 100 && time > 50 && !logEveryReptition) {
+                        println("Enabling logging every repetition after $i invocations (last invocation: ${time}ms)")
+                        logEveryReptition = true
                     }
 
                     if (time > 1000) {
                         println("cancelling on $i")
+                        break
                     }
                 }
             }
